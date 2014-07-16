88cc497a379affa8357b275c1b04e6a17638eef5
Sntanko <Shtanko-mephi@yandex.ru>
2014-07-17 02:41:57 +0400

    Arguments v2

--- FaceRec/main.cpp
+++ FaceRec/main.cpp
@@ -3,18 +3,15 @@
 #include <time.h>
 
 const std::string windowName = "Window";
-const std::string cascadePath = "haarcascade_frontalface_alt.xml";
+const std::string PathCascadeFrontalFace = "cascades\\haarcascade_frontalface_alt_tree.xml";
 
-const int INPUT_IMAGE_WIDTH = 864;
+const int INPUT_IMAGE_WIDTH = 640;
 const int INPUT_IMAGE_HEIGHT = 480;
 
-const int RESIZED_IMAGE_WIDTH = 800;
-const int RESIZED_IMAGE_HEIGHT = 450;
-
-const int WINDOW_WIDTH = 533;
-const int WINDOW_HEIGHT = 300;
-
-const int SIZE = 80;
+const double scaleFactor = 1.35;
+const int minNeighbors = 2;
+const int flags = 0;
+const int minSize = 40;
 
 const int N = 30; //FPS считается как среднее за N фреймов
 
@@ -26,8 +23,8 @@ int main(int argc, char **argv) {
 	if (!camera.isOpened()) return 1; //Если нельзя открыть камеру
 
 	//Каскад
-	cv::CascadeClassifier face_cascade(cascadePath);
-	if (face_cascade.empty()) return 1;
+	cv::CascadeClassifier cascadeFrontalFace(PathCascadeFrontalFace);
+	if (cascadeFrontalFace.empty()) return 1;
 
 	//Окно
 	cv::namedWindow(windowName);
@@ -37,6 +34,20 @@ int main(int argc, char **argv) {
 
 	int counter = 0; //счётчик для FPS
 	std::string FPS ="";
+	std::string cleanFPS ="";
+
+	for(int i = 0; i < N; i++) {
+		cv::Mat frame;
+		camera >> frame;
+	}
+	{
+		time = clock() - time;
+		int iFPS = ((double)N * CLOCKS_PER_SEC) / (time);
+		std::stringstream sstr;
+		sstr << iFPS;
+		cleanFPS = sstr.str();
+		time = clock();
+	}
 
 	for(;;) {
 
@@ -45,28 +56,17 @@ int main(int argc, char **argv) {
 		if (frame.empty()) continue; //если фрейм не считался
 
 		//обработка изображения
-		cv::Mat originalFrame = frame;
-		cv::resize(frame, frame, cv::Size(RESIZED_IMAGE_WIDTH,
-				RESIZED_IMAGE_HEIGHT));
 		cv::Mat grayFrame;
 		cv::cvtColor(frame, grayFrame, CV_BGR2GRAY);
 		cv::equalizeHist(grayFrame, grayFrame);
 
 		//поиск лиц
 		std::vector<cv::Rect> faces;
-		face_cascade.detectMultiScale(grayFrame, faces, 1.3, 4, 0, cv::Size(SIZE, SIZE));
-
-		//подго под размер экрана
-		cv::resize(originalFrame, originalFrame, cv::Size(WINDOW_WIDTH, WINDOW_HEIGHT));
+		cascadeFrontalFace.detectMultiScale(grayFrame, faces, scaleFactor, minNeighbors, flags, cv::Size(minSize, minSize));
 
 		//рисование
-		double scale = (double)WINDOW_WIDTH / (double)RESIZED_IMAGE_WIDTH;
-		for (unsigned int i = 0; i < faces.size(); i++) {
-			faces[i].x = (int)(scale * (double)faces[i].x);
-			faces[i].y = (int)(scale * (double)faces[i].y);
-			faces[i].width = (int)(scale * (double)faces[i].width);
-			faces[i].height = (int)(scale * (double)faces[i].height);
-			cv::rectangle(originalFrame, faces[i], cv::Scalar(0, 255, 0), 2);
+		for(unsigned int i = 0; i < faces.size(); i++) {
+			cv::rectangle(frame, faces[i], cv::Scalar(0, 255, 0), 2);
 		}
 
 		//подсчёт и рисование FPS
@@ -81,10 +81,14 @@ int main(int argc, char **argv) {
 		}
 		cv::Point p(10, 200);
 		cv::Scalar color(0, 255, 0);
-		cv::putText(originalFrame, FPS, p, cv::FONT_HERSHEY_DUPLEX, 1, color);
+		cv::putText(frame, cleanFPS + " " + FPS, p, cv::FONT_HERSHEY_DUPLEX, 1, color);
 
-		imshow(windowName, originalFrame);
-		if(cv::waitKey(30) >= 0) break;
+		imshow(windowName, frame);
+
+		if(cv::waitKey(1) >= 0) break;
 	}
+
+	cv::destroyAllWindows();
+	camera.release();
 
 }
